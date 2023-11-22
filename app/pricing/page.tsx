fb77330478f1bec3fb2c98af6865211faecb5516
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2023-11-22 12:55:08 +0300

    Implemented the pricing page.

---
+++ app/pricing/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Footer from "./footer/page";
+import Navbar from "./navbar/page";
+import FirstSection from "./section-01/page";
+import SecondSection from "./section-02/page";
+
+const PricingPage = () => {
+	return (
+		<div>
+			<Navbar />
+			<FirstSection />
+			<SecondSection />
+			<Footer />
+		</div>
+	)
+}
+
+export default PricingPage;
